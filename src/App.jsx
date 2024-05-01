fe4066d5c4113081a98f498abfb831ec6618b590
mrbillgates2001 <shahboznabiyev2001@gmail.com>
2024-05-01 14:21:53 +0500

    Portfoliyo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+	return (
+		<div>
+			<h1>This website will be available soon!</h1>
+		</div>
+	);
+};
+
+export default App;
